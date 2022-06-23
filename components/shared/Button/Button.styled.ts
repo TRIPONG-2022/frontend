e2d44c39a1775157b924c98099e7dafbfb8bf2e7
sangrimlee <contact@sangrim.dev>
2022-06-24 08:26:19 +0900

    refactor: Button Component 재사용을 고려한 수정
    - 재사용할 수 있게 css Props를 추가
    - 현재 Next.JS의 SWC에서는 cssProps Babel Configuration을 지원하지 않아서 따로 CSSProp props를 추가해주었습니다.
    - size랑 variant로 추상화

--- components/shared/Button/Button.styled.ts
+++ components/shared/Button/Button.styled.ts
@@ -1,14 +1,45 @@
-import styled from 'styled-components';
+import styled, { css, CSSProp, DefaultTheme } from 'styled-components';
 
-export const Button = styled.button`
-  width: 100%;
-  padding: 1rem;
-  border-radius: 1rem;
+const ButtonSizeStyles = {
+  md: css`
+    padding: 0.875rem 1rem;
+    border-radius: 0.5rem;
+    font-size: 0.875rem;
+  `,
+  lg: css`
+    padding: 1.25rem;
+    border-radius: 1rem;
+    font-size: 1rem;
+  `,
+};
 
-  font-size: 1rem;
+const ButtonVariantStyles = {
+  primary: css`
+    color: #ffffff;
+    background-color: #0dc5d6;
+  `,
+  outline: css`
+    color: #0dc5d6;
+    background-color: transparent;
+    box-shadow: inset 0 0 0 2px #0dc5d6;
+  `,
+};
+
+export type ButtonSize = keyof typeof ButtonSizeStyles;
+export type ButtonVariant = keyof typeof ButtonVariantStyles;
+
+interface BaseButtonProps {
+  $size: ButtonSize;
+  $variant: ButtonVariant;
+  $css?: CSSProp<DefaultTheme>;
+}
+
+export const BaseButton = styled.button<BaseButtonProps>`
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+
   font-weight: bold;
-  color: #ffffff;
-  background-color: #0dc5d6;
 
   &:hover {
     opacity: 0.75;
@@ -18,4 +49,10 @@ export const Button = styled.button`
     opacity: 0.25;
     pointer-events: none;
   }
+
+  ${({ $size, $variant, $css }) => css`
+    ${ButtonSizeStyles[$size]}
+    ${ButtonVariantStyles[$variant]}
+    ${$css}
+  `}
 `;
--- components/shared/Button/Button.tsx
+++ components/shared/Button/Button.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import { CSSProp, DefaultTheme } from 'styled-components';
 import * as Styled from './Button.styled';
 
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  css?: CSSProp<DefaultTheme>;
+  size?: Styled.ButtonSize;
+  variant?: Styled.ButtonVariant;
+}
 
-export default function Button({ children, ...buttonProps }: ButtonProps) {
+export default function Button({
+  css,
+  children,
+  size = 'md',
+  variant = 'primary',
+  ...buttonProps
+}: ButtonProps) {
   return (
-    <Styled.Button {...buttonProps}>
+    <Styled.BaseButton
+      $css={css}
+      $size={size}
+      $variant={variant}
+      {...buttonProps}
+    >
       <span>{children}</span>
-    </Styled.Button>
+    </Styled.BaseButton>
   );
 }
