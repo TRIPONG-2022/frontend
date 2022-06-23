d51737374d63d0018ba8d94ab994c4ee3a765ba6
seongjin <seongjin2427@gmail.com>
2022-06-23 15:14:15 +0900

    feat: join form 작성
    - 기본적인 회원가입 Form 작성 완료
    - nickName, loginId, email, password 유효성 검사 및 중복체크를 위한 로직
      작성
    - 알림바를 위한 JoinNotiBar 컴포넌트 작성
    
    * constants/schema.ts
    - JOIN_SCHEMA 작성
    
    * constants/message.ts
    - SCHEMA_MESSAGES 추가
    - DUPLICATE_MESSAGES 추가

--- constants/message.ts
+++ constants/message.ts
@@ -1,3 +1,20 @@
 export const SCHEMA_MESSAGES = {
   REQUIRED_FIELD: '필수 입력 정보입니다.',
+  NOT_MATCH_PASSWORD: '비밀번호가 일치하지 않습니다.',
+  WRONG_EMAIL_FORMAT: '잘못된 이메일 형식입니다.',
+  WRONG_NICKNAME_FORMAT: '닉네임에 특수문자가 포함되면 안됩니다!',
+  WRONG_LOGIND_FORMAT: '아이디에 특수문자 또는 한글이 포함되면 안됩니다!',
+} as const;
+
+type DUPLICATE_MESSAGE_TYPE = {
+  [key: string]: string;
+  DUPLICATE_NICKNAME: string;
+  DUPLICATE_LOGINID: string;
+  DUPLICATE_EMAIL: string;
+};
+
+export const DUPLICATE_MESSAGES: DUPLICATE_MESSAGE_TYPE = {
+  DUPLICATE_NICKNAME: '이미 사용중인 닉네임입니다.',
+  DUPLICATE_LOGINID: '이미 사용중인 아이디입니다.',
+  DUPLICATE_EMAIL: '이미 사용중인 이메일입니다.',
 } as const;
--- constants/schema.ts
+++ constants/schema.ts
@@ -25,4 +25,32 @@ export const LOGIN_SCHEMA = yup.object({
   password: yup.string().required(SCHEMA_MESSAGES.REQUIRED_FIELD),
 });
 
+export const JOIN_SCHEMA = yup.object({
+  nickName: yup
+    .string()
+    .matches(
+      /^[가-힣a-zA-Z0-9][^!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?\s]*$/,
+      SCHEMA_MESSAGES.WRONG_NICKNAME_FORMAT,
+    )
+    .required(SCHEMA_MESSAGES.REQUIRED_FIELD),
+  loginId: yup
+    .string()
+    .matches(
+      /^[a-z0-9][^ㄱ-ㅎ가-힣A-Z!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?\s]*$/,
+      SCHEMA_MESSAGES.WRONG_LOGIND_FORMAT,
+    )
+    .required(SCHEMA_MESSAGES.REQUIRED_FIELD),
+  email: yup
+    .string()
+    .email(SCHEMA_MESSAGES.WRONG_EMAIL_FORMAT)
+    .required(SCHEMA_MESSAGES.REQUIRED_FIELD),
+  password: yup.string().required(SCHEMA_MESSAGES.REQUIRED_FIELD),
+  passwordCheck: yup
+    .string()
+    .oneOf([yup.ref('password'), null], SCHEMA_MESSAGES.NOT_MATCH_PASSWORD)
+    .required(SCHEMA_MESSAGES.REQUIRED_FIELD),
+  notiBar: yup.string(),
+});
+
 export type LoginSchema = yup.InferType<typeof LOGIN_SCHEMA>;
+export type JoinSchema = yup.InferType<typeof JOIN_SCHEMA>;
