8be609521fe8a1d2f02af070fc22b5f547874e07
mini <jma1020@naver.com>
2022-06-28 14:22:49 +0900

    console 제거

--- hooks/useRegionFetch.ts
+++ hooks/useRegionFetch.ts
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 
 // 여기서 number 0으로 바꾸면 지역이 안뜨는 오류 발견 why??
+// 여기서 더 한다면, useSWR로 리팩토링 하거나, setState도 return값으로 err값도 return 값으로
 const useRegionFetch = (url: string, deps: string | number = 1) => {
   const [response, setResponse] = useState([]);
 
   useEffect(() => {
     (async function () {
+      if (!url) return;
       if (!deps) return;
 
       const res = await fetch(url);
@@ -13,7 +15,6 @@ const useRegionFetch = (url: string, deps: string | number = 1) => {
 
       setResponse(regcodes);
     })();
-    console.log('리렌더');
   }, [deps]);
 
   return { response };
