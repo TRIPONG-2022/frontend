5a6db207ced71129d1e65cc877556996f4674849
mini <jma1020@naver.com>
2022-06-29 15:39:50 +0900

    feat: Select 컴포넌트 export, styled  파일 추가
    - styled 지정 파일 별도로 생성
    - export 파일 별도로 생성

---
+++ components/shared/Select/Select.styled.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+
+export const Label = styled.label``;
+
+export const Select = styled.select``;
+
+export const Option = styled.option``;
+
+export const ErrorMessage = styled.p``;
