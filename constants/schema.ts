ee626b766bae2986ee4b0d7f23b5c28da6f0a2cd
sangrimlee <contact@sangrim.dev>
2022-06-29 19:06:11 +0900

    fix: Schema 관련 파일 충돌 해결
    - schema 파일 충돌 해결

--- constants/schema.ts
+++ constants/schema.ts
@@ -3,18 +3,25 @@ import { SCHEMA_MESSAGES } from './message';
 
 export const NICKNAME_SCHEMA = yup
   .string()
-  .min(2)
-  .max(20)
+  .matches(
+    /^[가-힣a-zA-Z0-9][^!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?\s]*$/,
+    SCHEMA_MESSAGES.WRONG_NICKNAME_FORMAT,
+  )
   .required(SCHEMA_MESSAGES.REQUIRED_FIELD);
+
 export const LOGIN_ID_SCHEMA = yup
   .string()
-  .min(2)
-  .max(20)
+  .matches(
+    /^[a-z0-9][^ㄱ-ㅎ가-힣A-Z!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?\s]*$/,
+    SCHEMA_MESSAGES.WRONG_LOGINID_FORMAT,
+  )
   .required(SCHEMA_MESSAGES.REQUIRED_FIELD);
+
 export const EMAIL_SCHEMA = yup
   .string()
-  .email()
+  .email(SCHEMA_MESSAGES.WRONG_EMAIL_FORMAT)
   .required(SCHEMA_MESSAGES.REQUIRED_FIELD);
+
 export const PASSWORD_CHEMA = yup
   .string()
   .matches(new RegExp(''))
@@ -26,40 +33,25 @@ export const LOGIN_SCHEMA = yup.object({
 });
 
 export const JOIN_SCHEMA = yup.object({
-  nickName: yup
-    .string()
-    .matches(
-      /^[가-힣a-zA-Z0-9][^!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?\s]*$/,
-      SCHEMA_MESSAGES.WRONG_NICKNAME_FORMAT,
-    )
-    .required(SCHEMA_MESSAGES.REQUIRED_FIELD),
-  loginId: yup
-    .string()
-    .matches(
-      /^[a-z0-9][^ㄱ-ㅎ가-힣A-Z!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?\s]*$/,
-      SCHEMA_MESSAGES.WRONG_LOGINID_FORMAT,
-    )
-    .required(SCHEMA_MESSAGES.REQUIRED_FIELD),
-  email: yup
-    .string()
-    .email(SCHEMA_MESSAGES.WRONG_EMAIL_FORMAT)
-    .required(SCHEMA_MESSAGES.REQUIRED_FIELD),
-  
+  nickName: NICKNAME_SCHEMA,
+  loginId: LOGIN_ID_SCHEMA,
+  email: EMAIL_SCHEMA,
+  notiBar: yup.string(),
+});
+
 export const RESET_PASSWORD_SCHEMA = yup.object({
-  password: yup.string().required(SCHEMA_MESSAGES.REQUIRED_FIELD),
+  password: PASSWORD_CHEMA,
   passwordCheck: yup
     .string()
     .oneOf([yup.ref('password'), null], SCHEMA_MESSAGES.NOT_MATCH_PASSWORD)
     .required(SCHEMA_MESSAGES.REQUIRED_FIELD),
-  notiBar: yup.string(),
 });
-
-export type JoinSchema = yup.InferType<typeof JOIN_SCHEMA>;
-export type LoginSchema = yup.InferType<typeof LOGIN_SCHEMA>;
-export type ResetPasswordSchema = yup.InferType<typeof RESET_PASSWORD_SCHEMA>;
 
 export const SEND_EMAIL_SCHEMA = yup.object({
   email: EMAIL_SCHEMA,
 });
 
+export type JoinSchema = yup.InferType<typeof JOIN_SCHEMA>;
+export type LoginSchema = yup.InferType<typeof LOGIN_SCHEMA>;
 export type SendEmailSchema = yup.InferType<typeof SEND_EMAIL_SCHEMA>;
+export type ResetPasswordSchema = yup.InferType<typeof RESET_PASSWORD_SCHEMA>;
