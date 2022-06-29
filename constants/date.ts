fd5981320b6ae0b376e13b321020bf14a9619c15
mini <jma1020@naver.com>
2022-06-29 15:46:11 +0900

    feat: Date 파일 date return 형식 변경
    - 변경한 Select 컴포넌트 형식에 맞게끔 date return 형식 변경

--- constants/date.ts
+++ constants/date.ts
@@ -1,23 +1,36 @@
-export const CurrentYear = new Date().getFullYear();
+export const getCurrentYear = () => new Date().getFullYear();
 
-export const years = Array(CurrentYear - (CurrentYear - 100))
-  .fill('')
-  .map((_, idx) => CurrentYear - idx);
+export const years = (currentYear: number) =>
+  new Array(currentYear - (currentYear - 100)).fill(undefined).map((_, i) => {
+    return {
+      value: currentYear - i,
+      label: currentYear - i + '년',
+    };
+  });
 
-export const months = new Array(12).fill(undefined).map((_, i) => i + 1);
+export const months = new Array(12).fill(undefined).map((_, i) => {
+  return {
+    value: i + 1,
+    label: i + 1 + '월',
+  };
+});
 
 export const LastDay = (year: number, month: number) => {
   return new Date(year, month, 0).getDate();
 };
 
-// useRef로 했을 때 예외처리인데 바꿔줘야지
-export const days = (year: number | undefined, month: number | undefined) => {
-  if (!year || !month) {
-    return;
-  }
-
-  return new Array(LastDay(year, month)).fill('').map((_, i) => i + 1);
+export const days = (year: number, month: number) => {
+  // if (typeof year == 'number' && typeof month == 'number') {
+  return new Array(LastDay(year, month)).fill(undefined).map((_, i) => {
+    return {
+      value: i + 1,
+      label: i + 1 + '일',
+    };
+  });
+  // }
 };
+
+// 지금 select의 특징??으로 defaultvalue가 맨첨에 들어가서 문제가 생김
 
 // 다른 방법 Array.from({length:12}, (_, i) => i +1)
 // 아직 Array from에 저 두번째 인자 역할 안찾아봄
