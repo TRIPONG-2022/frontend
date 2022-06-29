72451594ed82bc11ca15024f1059afe63d84c814
mini <jma1020@naver.com>
2022-06-29 22:48:47 +0900

    🔥 주석 삭제

--- constants/date.ts
+++ constants/date.ts
@@ -30,7 +30,5 @@ export const days = (year: number, month: number) => {
   // }
 };
 
-// 지금 select의 특징??으로 defaultvalue가 맨첨에 들어가서 문제가 생김
-
 // 다른 방법 Array.from({length:12}, (_, i) => i +1)
 // 아직 Array from에 저 두번째 인자 역할 안찾아봄
