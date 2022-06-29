25e12b0c43f0311d6cd24dac86c29303df15f6d6
mini <jma1020@naver.com>
2022-06-29 15:48:13 +0900

    feat: useRegionFetch 리팩토링
    - type 지정
    - 행정구역 data에 code와 name으로 이루어진 Map 객체 생성 (추후 backend로
      submit 시 활용)
    - Select 컴포넌트 형식에 맞춰서 data 구조 변경

--- hooks/useRegionFetch.ts
+++ hooks/useRegionFetch.ts
@@ -1,22 +1,53 @@
 import React, { useState, useEffect } from 'react';
 
-// 여기서 더 한다면, useSWR로 리팩토링 하거나, setState도 return값으로 err값도 return 값으로
-const useRegionFetch = (url: string, deps: string | number = 1) => {
-  const [response, setResponse] = useState([]);
+interface RegionData {
+  code: string;
+  name: string;
+}
+
+interface FormatData {
+  value: string;
+  label: string;
+}
+
+interface Props {
+  url: string;
+  deps?: string | undefined;
+  // 디펜던시 타입 찾아서 정리해보기
+}
+
+type UseRegionFetchReturns = [FormatData[], Map<string, string>];
+
+function useRegionFetch({ url, deps }: Props): UseRegionFetchReturns {
+  const [regionData, setRegionData] = useState<FormatData[]>([]);
+
+  const data = React.useMemo(() => {
+    const ret = new Map<string, string>();
+    regionData?.forEach(({ value, label }) => {
+      ret.set(value, label);
+    });
+    return ret;
+  }, [regionData]);
 
   useEffect(() => {
     (async function () {
-      if (!url) return;
-      if (!deps) return;
-
       const res = await fetch(url);
       const { regcodes } = await res.json();
 
-      setResponse(regcodes);
-    })();
-  }, [deps]);
+      const data = regcodes?.map((obj: RegionData) => {
+        return {
+          value: obj.code,
+          label: obj.name,
+        };
+      });
 
-  return { response };
-};
+      setRegionData(() => data as FormatData[]);
+    })();
+  }, [url, deps]);
+
+  return [regionData, data];
+}
 
 export default useRegionFetch;
+
+// response 를 아예 변환해줘라
