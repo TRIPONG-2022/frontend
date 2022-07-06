6689080ce800354a277e2e85e97824be4689c310
mini <jma1020@naver.com>
2022-07-06 19:20:29 +0900

    💄 스타일 추가

--- components/shared/Modal/Modal.styled.ts
+++ components/shared/Modal/Modal.styled.ts
@@ -12,16 +12,17 @@ export const Background = styled.div`
 `;
 
 export const ModalBox = styled.div`
-  position: fixed;
+  /* position: fixed;
   top: 50%;
-  left: 50%;
+  left: 50%; */
+
   z-index: 200;
   max-width: 32rem;
   min-width: 18.75rem;
   display: flex;
   flex-direction: column;
   align-items: center;
-  transform: translate(-50%, -50%);
+  /* transform: translate(-50%, -50%); */
   padding: 2.5rem 1rem;
   @media (min-width: 768px) {
     padding: 2.5rem 3rem;
@@ -39,4 +40,23 @@ export const ExitButton = styled.button`
   position: absolute;
   top: 1rem;
   right: 1rem;
+`;
+
+export const TitleText = styled.h1`
+  font-size: 2rem;
+`;
+
+export const DescriptionText = styled.p`
+  margin-top: 1rem;
+`;
+
+export const BtnContainer = styled.div`
+  width: 40%;
+`;
+
+export const TwoBtnContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  gap: 5rem;
 `;
