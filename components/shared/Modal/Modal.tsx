65f19d6b8c769f8edb3aa4fcb2f02d7ee2447a34
mini <jma1020@naver.com>
2022-07-06 19:20:03 +0900

    🚚 atoms 파일 컴포넌트 Modal 컴포넌트로 이동 및 선언

--- components/shared/Modal/Modal.tsx
+++ components/shared/Modal/Modal.tsx
@@ -1,8 +1,35 @@
 import React from 'react';
 import * as Styled from './Modal.styled';
-import { Title, Description, BtnContainer, BtnContainers } from './atoms';
 import styled from 'styled-components';
 import SVGIcon from '../SVGIcon';
+
+interface ChildrenProps {
+  children: React.ReactNode;
+}
+
+const Title = ({ children }: ChildrenProps) => {
+  return (
+    <>
+      <Styled.TitleText>{children}</Styled.TitleText>
+    </>
+  );
+};
+
+const Description = ({ children }: ChildrenProps) => {
+  return (
+    <>
+      <Styled.DescriptionText>{children}</Styled.DescriptionText>
+    </>
+  );
+};
+
+const BtnContainer = ({ children }: ChildrenProps) => {
+  return <Styled.BtnContainer>{children}</Styled.BtnContainer>;
+};
+
+const TwoBtnContainer = ({ children }: ChildrenProps) => {
+  return <Styled.TwoBtnContainer>{children}</Styled.TwoBtnContainer>;
+};
 
 interface ModalProps {
   isModal: boolean;
@@ -28,11 +55,21 @@ const ModalComponent = ({ isModal, close, children }: ModalProps) => {
   );
 };
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  justify-content: center;
+  align-items: center;
+  padding: 2rem;
+`;
 
 ModalComponent.Title = Title;
 ModalComponent.Description = Description;
 ModalComponent.BtnContainer = BtnContainer;
-ModalComponent.BtnContainers = BtnContainers;
+ModalComponent.BtnContainers = TwoBtnContainer;
 
 export default ModalComponent;
