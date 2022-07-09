960a8c3865c88015c230567a2ddcfc6273a0f6af
sangrimlee <contact@sangrim.dev>
2022-07-09 18:28:14 +0900

    feat: DateRageInput 추가
    - DateRangeInput 컴포넌트 추가

---
+++ components/editor/DateRangeInput/DateRangeInput.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import * as Styled from './DateRangeInput.styled';
+
+interface DateRangeInputProps {}
+
+export default function DateRangeInput({}: DateRangeInputProps) {
+  return <Styled.Container>DateRangeInput</Styled.Container>;
+}
