ccd8e0b83ad4f643aec1233e7bbe37691309b859
sangrimlee <contact@sangrim.dev>
2022-07-09 19:51:10 +0900

    feat: Publish Modal 추가

---
+++ components/editor/PublishModal/PublishModal.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import * as Styled from './PublishModal.styled';
+
+interface PublishModalProps {}
+
+export default function PublishModal({}: PublishModalProps) {
+  return <Styled.Container></Styled.Container>;
+}
