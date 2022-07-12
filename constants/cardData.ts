4251c9deba829f83e55bcb30568f944a02304308
mini <jma1020@naver.com>
2022-07-12 22:51:45 +0900

    🔨 임시 test data 생성

---
+++ constants/cardData.ts
@@ -0,0 +1,117 @@
+export const cardData = {
+  thumbnail:
+    'https://www.yeongnam.com/mnt/file/201809/20180914.010370820500001i1.jpg',
+  title: '여행',
+  description: '여행을 가고싶은 분들 모집합니다.',
+  tag: [
+    '태그1',
+    '태그2',
+    '태그3 31412412421321321',
+    '내가 이렇게 태그를 많이 달았다.',
+  ],
+  userName: 'username',
+  userImg:
+    'https://i.pinimg.com/236x/e2/b7/da/e2b7da6bc749ba2d7ebdfda28fac6009.jpg',
+  like: 23,
+  location: '제주도',
+  totalHeadCount: 1,
+  endDate: '2022-08-30',
+};
+
+export const cardList = [
+  {
+    thumbnail:
+      'https://www.yeongnam.com/mnt/file/201809/20180914.010370820500001i1.jpg',
+    title: '여행',
+    description: '여행을 가고싶은 분들 모집합니다.',
+    tag: [
+      '태그1',
+      '태그2',
+      '태그3 31412412421321321',
+      '내가 이렇게 태그를 많이 달았다.',
+    ],
+    userName: 'username',
+    userImg:
+      'https://i.pinimg.com/236x/e2/b7/da/e2b7da6bc749ba2d7ebdfda28fac6009.jpg',
+    like: 23,
+    location: '제주도',
+    totalHeadCount: 1,
+    endDate: '2022-08-30',
+  },
+  {
+    thumbnail:
+      'https://www.yeongnam.com/mnt/file/201809/20180914.010370820500001i1.jpg',
+    title: '모집',
+    description: '여행을 가고싶은 분들 모집합니다.',
+    tag: [
+      '태그1',
+      '태그2',
+      '태그3 31412412421321321',
+      '내가 이렇게 태그를 많이 달았다.',
+    ],
+    userName: 'username',
+    userImg:
+      'https://i.pinimg.com/236x/e2/b7/da/e2b7da6bc749ba2d7ebdfda28fac6009.jpg',
+    like: 23,
+    location: '경상도',
+    totalHeadCount: 1,
+    endDate: '2022-05-30',
+  },
+  {
+    thumbnail:
+      'https://www.yeongnam.com/mnt/file/201809/20180914.010370820500001i1.jpg',
+    title: '여행',
+    description: '여행을 가고싶은 분들 모집합니다.',
+    tag: [
+      '태그1',
+      '태그2',
+      '태그3 31412412421321321',
+      '내가 이렇게 태그를 많이 달았다.',
+    ],
+    userName: 'username',
+    userImg:
+      'https://i.pinimg.com/236x/e2/b7/da/e2b7da6bc749ba2d7ebdfda28fac6009.jpg',
+    like: 23,
+    location: '경기도',
+    totalHeadCount: 0,
+    endDate: '2022-08-30',
+  },
+  {
+    thumbnail:
+      'https://www.yeongnam.com/mnt/file/201809/20180914.010370820500001i1.jpg',
+    title: '여행',
+    description: '여행을 가고싶은 분들 모집합니다.',
+    tag: [
+      '태그1',
+      '태그2',
+      '태그3 31412412421321321',
+      '내가 이렇게 태그를 많이 달았다.',
+    ],
+    userName: 'username',
+    userImg:
+      'https://i.pinimg.com/236x/e2/b7/da/e2b7da6bc749ba2d7ebdfda28fac6009.jpg',
+    like: 23,
+    location: '서울',
+    totalHeadCount: 1,
+    endDate: '2022-08-30',
+  },
+  {
+    thumbnail:
+      'https://www.yeongnam.com/mnt/file/201809/20180914.010370820500001i1.jpg',
+    title: '여행',
+    description: '여행을 가고싶은 분들 모집합니다.',
+    tag: [
+      '태그1',
+      '태그2',
+      '태그3 31412412421321321',
+      '내가 이렇게 태그를 많이 달았다.',
+    ],
+    userName: 'username',
+    userImg:
+      'https://i.pinimg.com/236x/e2/b7/da/e2b7da6bc749ba2d7ebdfda28fac6009.jpg',
+    like: 23,
+    location: '남양주',
+    totalHeadCount: 1,
+    endDate: '2022-08-30',
+  },
+];
