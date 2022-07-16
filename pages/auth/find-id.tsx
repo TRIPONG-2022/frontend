9e143e7c1eacb0298a44c96dc2a23277face02fa
sangrimlee <contact@sangrim.dev>
2022-07-16 18:53:30 +0900

    feat: 아이디 찾기 페이지 에러 메시지 추가
    - 아이디 찾기 에러메시지추가

--- pages/auth/find-id.tsx
+++ pages/auth/find-id.tsx
@@ -1,11 +1,18 @@
 import type { NextPage } from 'next';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { SendEmailSchema, SEND_EMAIL_SCHEMA } from '@/constants/schema';
 import AuthLayout from '@/layouts/AuthLayout';
 import AuthInput from '@/components/shared/AuthInput';
+import Button from '@/components/shared/Button';
+import { requestFindID } from '@/api/auth';
 
 const FindIDPage: NextPage = () => {
+  const router = useRouter();
+  const [error, setError] = useState<string>('');
+  const [isSuccessFindID, setIsSuccessFindID] = useState<boolean>(false);
   const {
     register,
     handleSubmit,
@@ -15,15 +22,18 @@ const FindIDPage: NextPage = () => {
     resolver: yupResolver(SEND_EMAIL_SCHEMA),
   });
 
-  const onSubmit = (data: SendEmailSchema) => {
-    console.log(data);
-    alert(JSON.stringify(data));
+  const onSubmit = async ({ email }: SendEmailSchema) => {
+    const { isError, error, data } = await requestFindID(email);
+    if (error) {
+      setError(error);
+    }
   };
 
   return (
     <AuthLayout
       title="아이디 찾기"
       description="이메일 주소로 가입하신 아이디가 전송됩니다."
+      errorMessage={error}
     >
       <form onSubmit={handleSubmit(onSubmit)}>
         <AuthInput
@@ -33,13 +43,18 @@ const FindIDPage: NextPage = () => {
           errorMessage={errors.email?.message}
           {...register('email')}
         />
-        <button
+        <Button
+          size="lg"
           type="submit"
+          css={`
+            width: 100%;
+            margin-top: 1rem;
+          `}
           disabled={!isValid || !isDirty}
           aria-disabled={!isValid || !isDirty}
         >
-          이메일 전송
-        </button>
+          아이디 찾기
+        </Button>
       </form>
     </AuthLayout>
   );
