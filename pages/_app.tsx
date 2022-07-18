5f5589115f8cbcf074b97ab5492b13528ef8eca0
mini <jma1020@naver.com>
2022-07-18 15:42:26 +0900

    ⚡️ React-Query _app.js 설정 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,28 @@ import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
 import theme from '@/styles/theme';
 import GlobalStyle from '@/styles/global';
+import { QueryClient, QueryClientProvider, Hydrate } from 'react-query';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnMount: false,
+        refetchOnReconnect: false,
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
+
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </Hydrate>
+    </QueryClientProvider>
   );
 }
 
