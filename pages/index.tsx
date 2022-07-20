d6270f70354dced24a72491a4b04420de199a1e6
mini <jma1020@naver.com>
2022-07-20 18:58:46 +0900

    🔥 불필요한 코드 삭제

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,13 @@
 import CardContainer from '@/components/shared/Card/CardContainer';
-import CardExample from '@/components/shared/Card/CardExample';
-import CardPresenter from '@/components/shared/Card/CardExample2';
-import CardTextGrid from '@/components/shared/Card/CardPresentation';
 import MainLayout from '@/layouts/MainLayout';
 import type { NextPage } from 'next';
 import React from 'react';
-import styled from 'styled-components';
 
 const HomePage: NextPage = () => {
   return (
     <MainLayout>
       <h1 className="">TRIPONG</h1>
 
-      {/* 추후에 props로 조건에 맞는 post data를 props로 넘겨받을 예정 */}
       <CardContainer />
     </MainLayout>
   );
