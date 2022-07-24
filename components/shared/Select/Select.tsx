5400eb616a2e9609445e84644221ad50f9ab4e3e
mini <jma1020@naver.com>
2022-07-24 17:27:29 +0900

    ⚡️ Custom Select 사용을 위한 설정 추가

--- components/shared/Select/Select.tsx
+++ components/shared/Select/Select.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useRef, useState, RefObject } from 'react';
+import styled from 'styled-components';
 import * as Styled from './Select.styled';
 
 interface SelectOption {
@@ -10,16 +11,29 @@ interface SelectProps extends React.SelectHTMLAttributes<HTMLSelectElement> {
   label?: string;
   errorMessage?: string;
   defaultLabel?: string;
+  setValue: any;
   options?: SelectOption[];
 }
 
 const Select = React.forwardRef<HTMLSelectElement, SelectProps>(
-  ({ label, id, options, defaultLabel, ...selectProps }, ref) => {
+  ({ label, id, options, defaultLabel, setValue, ...selectProps }, ref) => {
+    const [isOpen, setOpen] = useState(false);
+    const [selectedValue, setselectedValue] = useState(defaultLabel);
+
+    useEffect(() => {
+      // 이렇게 사용한 이유는 이미 select에서 ref를 사용 중이라 이렇게 tag를 가져옴
+      const selectTag = document.getElementById(id as string);
+
+      if (selectTag) {
+        (selectTag as HTMLInputElement).value = selectedValue as string;
+      }
+    }, [selectedValue]);
+
     return (
       <Styled.Container>
         {label && <Styled.Label htmlFor={id}>{label}</Styled.Label>}
 
-        <Styled.Select {...selectProps} ref={ref}>
+        <Styled.Select id={id} {...selectProps} ref={ref}>
           <Styled.Option value="defaultValue"> {defaultLabel} </Styled.Option>
 
           {options?.map(({ value, label }) => (
@@ -28,10 +42,56 @@ const Select = React.forwardRef<HTMLSelectElement, SelectProps>(
             </Styled.Option>
           ))}
         </Styled.Select>
+
+        <CustomDivContainer
+          onClick={() => {
+            setOpen((prev) => !prev);
+          }}
+        >
+          <div className={`custom-select ${isOpen && 'open'}`}>
+            <div className="custom-select__trigger">
+              <span>
+                {options?.find((item) => item.value === selectedValue)?.label ||
+                  'Select'}
+              </span>
+              <div className="arrow"></div>
+            </div>
+            <div className="custom-options">
+              {options?.map((item) => (
+                <div
+                  key={item.value}
+                  onClick={() => {
+                    setselectedValue(item?.value as string);
+                    setValue(id, item?.value, {
+                      shouldValidate: true,
+                      shouldDirty: true,
+                    });
+                  }}
+                  className="option-container"
+                >
+                  <span
+                    className={`custom-option ${
+                      selectedValue === item.value && 'selected'
+                    } `}
+                    data-value={item.value}
+                  >
+                    {item.label}
+                  </span>
+                </div>
+              ))}
+            </div>
+          </div>
+        </CustomDivContainer>
       </Styled.Container>
     );
   },
 );
+
+const CustomDivContainer = styled.div`
+  position: relative;
+  width: 100%;
+`;
+const CumtomDivTitle = styled.div``;
 
 Select.displayName = 'Select';
 
