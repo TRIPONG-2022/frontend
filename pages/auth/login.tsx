81fa11c3f5dc409bed54f1dba587f4a7ef59e2b9
mini <jma1020@naver.com>
2022-07-27 15:27:49 +0900

    🔥 사용하지 않는 코드 삭제

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -23,28 +23,9 @@ const LoginPage: NextPage = () => {
     resolver: yupResolver(LOGIN_SCHEMA),
   });
 
-  async function postData(url = '', data = {}) {
-    // 옵션 기본 값은 *로 강조
-    const response = await fetch(url, {
-      method: 'POST', // *GET, POST, PUT, DELETE 등
-      mode: 'cors', // no-cors, *cors, same-origin
-      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: 'same-origin', // include, *same-origin, omit
-      headers: {
-        'Content-Type': 'application/json',
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      redirect: 'follow', // manual, *follow, error
-      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      body: JSON.stringify(data), // body의 데이터 유형은 반드시 "Content-Type" 헤더와 일치해야 함
-    });
-    return response.json(); // JSON 응답을 네이티브 JavaScript 객체로 파싱
-  }
-
   const onSubmit = (data: LoginSchema) => {
     console.log(data);
     login(data);
-    router.push('/');
   };
 
   // 로그인 시 token을 받고,  token있을 경우에
