a4d319c352dc0733adaed585163623f2ab8aef56
mini <jma1020@naver.com>
2022-07-30 19:15:15 +0900

    fix: 더미 데이터 삭제 및 Props 에러로 인한 임시 데이터 Index page에 생성
    - constants 더미 데이터 삭제
    - Props 에러로 인해 임시 데이터 Index Page에 생성

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,6 @@ import MainLayout from '@/layouts/MainLayout';
 import CardList from '@/components/shared/CardList';
 import useScreenType from '@/hooks/useScreenType';
 
-import { CARD_LIST } from '@/constants/cardData';
-
 const HomePage: NextPage = () => {
   const { isDesktop, isTablet, isMobile } = useScreenType();
 
@@ -13,6 +11,25 @@ const HomePage: NextPage = () => {
     () => (isDesktop ? 3 : isTablet ? 2 : isMobile),
     [isTablet, isDesktop, isMobile],
   );
+
+  const CARD_LIST = [
+    {
+      id: 2,
+      category: 'board',
+      thumbnail:
+        'https://www.yeongnam.com/mnt/file/201809/20180914.010370820500001i1.jpg',
+      title: '여행',
+      description: '여행을 가고싶은 분들 모집합니다.',
+      tag: ['태그1', '태그2', '태그3', '내가 이렇게 태그를 많이 달았다.'],
+      userName: 'username',
+      userImg:
+        'https://i.pinimg.com/236x/e2/b7/da/e2b7da6bc749ba2d7ebdfda28fac6009.jpg',
+      like: 23,
+      location: '제주도',
+      totalHeadCount: 1,
+      endDate: '2022-08-30',
+    },
+  ];
 
   return (
     <MainLayout>
