4b65010eae6b416e0783511d09b1ca3dc3dff132
mini <jma1020@naver.com>
2022-07-31 10:12:53 +0900

    feature: userSlice data 형태 생성 및 saveUser 수정
    - initialState 수정
    - saveUser 수정

--- store/slice/userSlice.ts
+++ store/slice/userSlice.ts
@@ -1,13 +1,24 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { Stats } from 'fs';
 
-const initialState = { user: '' };
+const initialState = {
+  authentication: false,
+  name: '',
+  nickName: '',
+  picture: '',
+  isLogIn: false,
+};
 
 const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
     saveUser(state, action) {
-      state.user = action.payload.user;
+      state.authentication = action.payload.authentication == 1 ? true : false;
+      state.name = action.payload.name;
+      state.nickName = action.payload.nickName;
+      state.picture = action.payload.picture;
+      state.isLogIn = action.payload.isLogIn;
     },
   },
 });
