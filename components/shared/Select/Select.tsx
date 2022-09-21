da104847eadb9e60f2938fdac8e644121e37e08c
mini <jma1020@naver.com>
2022-09-21 23:23:45 +0900

    feat: Select 컴포넌트 수정
    - Styled 컴포넌트 네이밍 변경
    - onChangeOption props 수정
    - 리팩토링

--- components/shared/Select/Select.tsx
+++ components/shared/Select/Select.tsx
@@ -1,19 +1,19 @@
-import React, { useEffect, useRef, useState, RefObject } from 'react';
-import styled, { css } from 'styled-components';
+import React, { useState } from 'react';
+import SVGIcon from '../SVGIcon';
 import * as Styled from './Select.styled';
-import { UseFormSetValue, UseFormReturn } from 'react-hook-form';
 
 interface SelectOption {
   value: string | number;
   label: string | number;
 }
+
 interface SelectProps {
   label?: string;
   id: string;
   errorMessage?: string;
-  defaultLabel?: string;
+  defaultLabel: string;
   options?: SelectOption[];
-  onClickOption?: any;
+  onChangeOption: (value: string | number) => void;
 }
 
 const Select = ({
@@ -21,52 +21,53 @@ const Select = ({
   id,
   options,
   defaultLabel,
-  onClickOption,
+  onChangeOption,
 }: SelectProps) => {
-  const [isOpen, setOpen] = useState(false);
-  const [selectedValue, setselectedValue] = useState(defaultLabel);
+  const [isOpen, setOpen] = useState<boolean>(false);
+  const [selectedOption, setSelectedOption] = useState<SelectOption | null>(
+    null,
+  );
+
+  const handleClick = (option: SelectOption) => {
+    setSelectedOption(option);
+    onChangeOption(option.value);
+  };
 
   return (
     <Styled.Container>
       <Styled.Label htmlFor={id}>{label}</Styled.Label>
-
-      <Styled.CustomDivContainer
-        type="button"
-        onClick={() => {
-          setOpen((prev) => !prev);
-        }}
-      >
-        <Styled.CustomDivTitle isOpen={isOpen}>
-          {options?.find((item) => item.value === selectedValue)?.label ||
-            defaultLabel}
-
-          {/* <Arrow /> */}
-        </Styled.CustomDivTitle>
-
-        <Styled.CustomUl isOpen={isOpen}>
-          {options?.map((item) => (
-            <Styled.CustomLi
-              key={item.value}
-              onClick={() => {
-                setselectedValue(item?.value as string);
-                onClickOption({ id, value: item?.value });
-              }}
-            >
-              <span data-value={item.value}>{item.label}</span>
-            </Styled.CustomLi>
-          ))}
-        </Styled.CustomUl>
-      </Styled.CustomDivContainer>
       {isOpen && (
-        <Styled.Back
+        <Styled.Backdrop
           onClick={() => {
             setOpen(false);
           }}
         />
       )}
+      <Styled.OptionContainer
+        type="button"
+        onClick={() => {
+          setOpen((prev) => !prev);
+        }}
+      >
+        <Styled.OptionTitle isOpen={isOpen}>
+          {selectedOption === null ? defaultLabel : selectedOption.label}
+          <SVGIcon icon="ChevronDownIcon" size={16} />
+        </Styled.OptionTitle>
+        <Styled.OptionList isOpen={isOpen}>
+          {options?.map((option) => (
+            <Styled.OptionItem
+              key={option.value}
+              onClick={() => {
+                handleClick(option);
+              }}
+            >
+              <span>{option.label}</span>
+            </Styled.OptionItem>
+          ))}
+        </Styled.OptionList>
+      </Styled.OptionContainer>
     </Styled.Container>
   );
 };
-Select.displayName = 'Select';
 
 export default Select;
