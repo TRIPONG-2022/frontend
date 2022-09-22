c6b390e9f661df9819ae535939ebd0252e4aec32
sangrimlee <contact@sangrim.dev>
2022-09-22 18:14:15 +0900

    feat: PostList 컴포넌트 추가
    - PostList 컴포넌트 추가
    - size Props를 통한 스크린 크기별 칼럼 개수 변경 가능하도록 수정

---
+++ components/post/PostList/PostList.styled.ts
@@ -0,0 +1,34 @@
+import styled, { css } from 'styled-components';
+import { SCREEN_DESKTOP, SCREEN_TABLET } from '@/styles/screen';
+
+const PostListSizeStyles = {
+  md: css`
+    grid-template-columns: repeat(1, minmax(0, 1fr));
+
+    ${SCREEN_DESKTOP} {
+      grid-template-columns: repeat(2, minmax(0, 1fr));
+    }
+  `,
+  lg: css`
+    grid-template-columns: repeat(1, minmax(0, 1fr));
+
+    ${SCREEN_TABLET} {
+      grid-template-columns: repeat(2, minmax(0, 1fr));
+    }
+
+    ${SCREEN_DESKTOP} {
+      grid-template-columns: repeat(3, minmax(0, 1fr));
+    }
+  `,
+};
+
+export type PostListSize = keyof typeof PostListSizeStyles;
+
+export const PostListContainer = styled.ul<{
+  size: PostListSize;
+}>`
+  display: grid;
+  gap: 1rem;
+
+  ${({ size }) => PostListSizeStyles[size]};
+`;
---
+++ components/post/PostList/PostList.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { Post } from '@/types/post';
+
+import PostItem from '../PostItem';
+import * as Styled from './PostList.styled';
+
+interface PostListProps {
+  posts: Post[];
+  size?: Styled.PostListSize;
+}
+
+/**
+ * PostList 컴포넌트
+ *
+ * size: 스크린 사이즈 별 칼럼 개수
+ * - md: Mobile 1, Tablet ~ 2
+ * - lg: Mobile 1, Tablet 2, Desktop 3
+ */
+export default function PostList({ posts, size = 'md' }: PostListProps) {
+  return (
+    <Styled.PostListContainer size={size}>
+      {posts.map((post, index) => (
+        <li key={`${post.id}${index}`}>
+          <PostItem post={post} />
+        </li>
+      ))}
+    </Styled.PostListContainer>
+  );
+}
