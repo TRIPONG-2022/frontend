bcca537ec71245f6be08319f0e52e17555b00c1e
sangrimlee <contact@sangrim.dev>
2022-09-29 09:44:26 +0900

    feat: PostPage 추가
    - Post 페이지 추가

--- pages/posts/[...param].tsx
+++ pages/posts/[...param].tsx
@@ -1,4 +1,15 @@
+import React from 'react';
+import Head from 'next/head';
+import { useQuery } from 'react-query';
 import type { GetServerSideProps, NextPage } from 'next';
+
+import MainLayout from '@/layouts/MainLayout';
+import PostBody from '@/components/post/PostBody';
+import PostHeader from '@/components/post/PostHeader';
+import PostNotFound from '@/components/post/PostNotFound';
+
+import { Post } from '@/types/post';
+import { requestGetPost } from '@/api/post';
 import { checkIsValidPostPageParam, handlePostPageParam } from '@/utils/post';
 
 interface PostPageProps {
@@ -7,25 +18,53 @@ interface PostPageProps {
 }
 
 const PostPage: NextPage<PostPageProps> = ({ category, postId }) => {
-  return <div>{category + postId}</div>;
+  const { data: post } = useQuery<Post>(['post', category, postId], () =>
+    requestGetPost(category, postId),
+  );
+
+  if (!post) {
+    return (
+      <MainLayout>
+        <PostNotFound />
+      </MainLayout>
+    );
+  }
+
+  return (
+    <React.Fragment>
+      <Head>
+        <title>{post.title} | TRIPONG</title>
+      </Head>
+      <MainLayout>
+        <PostHeader post={post} />
+        <PostBody post={post} />
+      </MainLayout>
+    </React.Fragment>
+  );
 };
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const { param } = context.query;
-  const [category, postId] = handlePostPageParam(param);
+  try {
+    const { param } = context.query;
+    const [category, postId] = handlePostPageParam(param);
 
-  if (!checkIsValidPostPageParam(category, postId)) {
+    if (!checkIsValidPostPageParam(category, postId)) {
+      return {
+        notFound: true,
+      };
+    }
+
+    return {
+      props: {
+        category,
+        postId,
+      },
+    };
+  } catch (error) {
     return {
       notFound: true,
     };
   }
-
-  return {
-    props: {
-      category,
-      postId,
-    },
-  };
 };
 
 export default PostPage;
