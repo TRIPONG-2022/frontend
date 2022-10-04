7305de381759c5fc5a6aa352350f290fb50bab5f
mini <jma1020@naver.com>
2022-10-05 07:32:53 +0900

    feat: BlackUser도 무한스크롤 형식으로 변경
    - useInfinity query 사용
    - IntersectionObserver 사용

--- components/admin/ManagedUser/ManagedUserList/ManagedBlackUserList.tsx
+++ components/admin/ManagedUser/ManagedUserList/ManagedBlackUserList.tsx
@@ -1,16 +1,40 @@
-import useManagedBlackUserQuery from '@/hooks/useManagedBlackUserQuery';
-import ManagedUserCard from '../ManagedUserCard/ManagedUserCard';
+import React, { SetStateAction, useCallback } from 'react';
 
-const ManagedBlackUserList = () => {
-  const { data, isLoading, isError } = useManagedBlackUserQuery();
+import { SearchUserParams } from '@/types/search-params';
+import { ManagedUserInterface } from '@/types/managed-user';
+import useManagedBlackUserQuery from '@/components/admin/ManagedUser/hooks/useManagedBlackUserQuery';
+
+import ManagedUserCard from '../ManagedUserCard/ManagedUserCard';
+import ObserverBottom from './ObserverBottom';
+
+interface Props {
+  searchParams: SearchUserParams;
+  setSearchParams: React.Dispatch<SetStateAction<SearchUserParams>>;
+}
+
+const ManagedUserList = ({ searchParams, setSearchParams }: Props) => {
+  const { data, isLoading, isError, fetchNextPage, hasNextPage } =
+    useManagedBlackUserQuery(searchParams);
+
+  const getNextPage = useCallback(() => {
+    setSearchParams((prev) => ({ ...prev, page: prev.page + 1 }));
+    fetchNextPage();
+  }, [setSearchParams, fetchNextPage]);
+
+  if (isLoading) return <div>로딩 중</div>;
+
+  if (isError) return <div>에러발생</div>;
 
   return (
     <>
-      {data?.map((data) => (
-        <ManagedUserCard userData={data} key={data.id} />
-      ))}
+      {data?.pages.map(({ content }) =>
+        content?.map((data: ManagedUserInterface) => (
+          <ManagedUserCard userData={data} key={data.id} />
+        )),
+      )}
+      {hasNextPage && <ObserverBottom getNextPage={getNextPage} />}
     </>
   );
 };
 
-export default ManagedBlackUserList;
+export default ManagedUserList;
--- components/admin/ManagedUser/hooks/useManagedBlackUserQuery.ts
+++ components/admin/ManagedUser/hooks/useManagedBlackUserQuery.ts
@@ -1,27 +1,22 @@
-import { useQuery } from 'react-query';
+import { useInfiniteQuery } from 'react-query';
 import { AxiosError } from 'axios';
 
-import { getReportUsers } from '@/api/admin';
 import { SearchUserParams } from '@/types/search-params';
+import { getReportUsers } from '@/api/admin';
 
-interface ManagedUserInterface {
-  content: {
-    id: number;
-    name: string;
-    loginId: string;
-    nickName: string;
-    createdDate: string;
-    roles: { roleName: string }[];
-    reportType?: string;
-    reporterName?: string;
-  }[];
-}
-
-function useManagedBlackUserQuery(params: SearchUserParams) {
-  const query = useQuery<ManagedUserInterface, AxiosError>(
-    ['blackUserList', params.page],
-    () => getReportUsers(params),
+function useManagedUserQuery(params: SearchUserParams) {
+  const query = useInfiniteQuery(
+    'userList',
+    ({ pageParam = 0 }) => {
+      return getReportUsers({ ...params, page: pageParam });
+    },
     {
+      getNextPageParam: (lastPage, pages) => {
+        if (lastPage.totalPages <= pages.length) return undefined;
+
+        return pages.length;
+      },
+
       onError: (err: AxiosError) => {
         if (err.response?.status) {
           if (500 <= err.response?.status) {
@@ -37,6 +32,5 @@ function useManagedBlackUserQuery(params: SearchUserParams) {
 
   return query;
 }
-1;
 
-export default useManagedBlackUserQuery;
+export default useManagedUserQuery;
