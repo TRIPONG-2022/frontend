f286c18afed0d61aa9fee5a1e85ec892bdafb344
mini <jma1020@naver.com>
2022-10-15 02:29:20 +0900

    feat: ManagedPost, ManagedUser List 컴포넌트 grid 추가

--- components/admin/ManagedPost/ManagedPostList/ManagedPostList.styled.ts
+++ components/admin/ManagedPost/ManagedPostList/ManagedPostList.styled.ts
@@ -1,8 +1,19 @@
 import styled from 'styled-components';
+import { SCREEN_DESKTOP } from '@/styles/screen';
 
 export const ManagedPostListContainer = styled.ul`
   display: grid;
-
-  grid-template-columns: repeat(2, minmax(0, 1fr));
   gap: 0.5rem;
+  grid-template-columns: repeat(2, minmax(0, 1fr));
+
+  @media (max-width: 580px) {
+    grid-template-columns: repeat(1, minmax(0, 1fr));
+  }
+
+  ${SCREEN_DESKTOP} {
+    grid-template-columns: repeat(3, minmax(0, 1fr));
+  }
+  @media (min-width: 1920px) {
+    grid-template-columns: repeat(4, minmax(0, 1fr));
+  }
 `;
