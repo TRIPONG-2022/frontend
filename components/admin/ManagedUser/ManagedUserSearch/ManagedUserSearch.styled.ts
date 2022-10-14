124a20e40fad9e37d48f5cf01cf1b716daa6b7b0
mini <jma1020@naver.com>
2022-10-15 02:30:29 +0900

    feat: ManagedUserSearch 컴포넌트 style 변경

--- components/admin/ManagedUser/ManagedUserSearch/ManagedUserSearch.styled.ts
+++ components/admin/ManagedUser/ManagedUserSearch/ManagedUserSearch.styled.ts
@@ -3,27 +3,54 @@ import styled from 'styled-components';
 export const ManagedUserSearchContainer = styled.div`
   display: flex;
   gap: 1rem;
-  justify-content: flex-start;
+
   align-items: center;
   margin-bottom: 2rem;
 `;
 
 export const SelectWrapper = styled.div`
-  min-width: 10rem;
+  min-width: 9rem;
+  flex-shrink: 3;
+`;
+
+export const SearchInputWrapper = styled.div`
+  position: relative;
 `;
 
 export const SearchInput = styled.input`
-  border: 2px solid;
-  border-radius: 1rem;
+  border: 2px solid white;
+  border-radius: 1.5rem;
   padding: 1.25rem;
+  padding-inline-start: 3rem;
 
   font-size: 1rem;
-  color: #000000;
+  background: ${({ theme }) => theme.colors.gray[100]};
+
+  outline: none;
+  box-sizing: content-box;
+  transition: all 0.5s;
+
+  ::-webkit-search-cancel-button {
+    display: none;
+  }
+
+  :focus {
+    background-color: #fff;
+    border-color: ${({ theme }) => theme.colors.primary.hex};
+    box-shadow: 0 0 5px rgba(109, 207, 246, 0.5);
+  }
+
+  @media (max-width: 500px) {
+    width: 6rem;
+    :focus {
+      width: 8rem;
+    }
+  }
 `;
 
-export const SearchButton = styled.button`
-  padding: 1rem 1.5rem;
-  border-radius: 1rem;
-
-  background-color: rgba(${({ theme }) => theme.colors.primary.rgb}, 0.4);
+export const SearchIconWrapper = styled.div`
+  position: absolute;
+  left: 1rem;
+  top: 50%;
+  transform: translate(0, -55%);
 `;
