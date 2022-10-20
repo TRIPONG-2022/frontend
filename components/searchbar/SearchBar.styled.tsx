9f0c8bf4fbd05fdd76ed22c0eeea4d145693683f
mini <jma1020@naver.com>
2022-10-20 21:52:59 +0900

    feat: SearchBar UI 수정
    - SearchType 으로 인한 UI 수정

--- components/searchbar/SearchBar.styled.tsx
+++ components/searchbar/SearchBar.styled.tsx
@@ -1,43 +1,69 @@
+import { HEADER_HEIGHT } from '@/constants/menus';
 import styled from 'styled-components';
 
 export const SearchBarContainer = styled.div`
   position: fixed;
-  top: 0.375rem;
+  top: 0;
   left: 0;
   right: 0;
+  height: ${HEADER_HEIGHT};
   z-index: 9999;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+
+  border-bottom: solid 1px black;
+  background-color: white;
 `;
 
+export const SelectWrapper = styled.div`
+  min-width: 10rem;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 1rem;
+`;
+export const DeleteIconWrapper = styled.div`
+  margin-left: 1rem;
+`;
+
+export const SearchInputWrapper = styled.div`
+  position: relative;
+
+  flex-grow: 1;
+  max-width: 600px;
+`;
 export const SearchInput = styled.input`
-  width: calc(100% - 7rem);
+  width: 100%;
+
   border: 2px solid;
   border-radius: 1.5rem;
-  padding: 1.25rem 3.5rem;
+  padding: 1.125rem 3.5rem 1.125rem 1rem;
 
   font-size: 1.25rem;
   font-weight: 600;
   outline: none;
-  box-sizing: content-box;
+
   transition: all 0.5s;
   ::-webkit-search-cancel-button {
     display: none;
   }
 
-  background-color: #fff;
-  border-color: ${({ theme }) => theme.colors.primary.hex};
-  box-shadow: 0 0 5px rgba(109, 207, 246, 0.5);
+  background-color: ${({ theme }) => theme.colors.gray[50]};
+  border-color: white;
+  :focus {
+    background-color: #fff;
+    border-color: ${({ theme }) => theme.colors.primary.hex};
+    box-shadow: 0 0 5px rgba(109, 207, 246, 0.5);
+  }
 `;
 
 export const SearchIconWrapper = styled.div`
   position: absolute;
   right: 1rem;
-  top: 50%;
-  transform: translate(0, -55%);
-`;
-
-export const DeleteIconWrapper = styled.div`
-  position: absolute;
-  left: 1rem;
   top: 50%;
   transform: translate(0, -55%);
 `;
--- components/searchbar/SearchBar.tsx
+++ components/searchbar/SearchBar.tsx
@@ -1,7 +1,9 @@
 import { useRouter } from 'next/router';
+import { useState } from 'react';
 
 import Portal from '@/components/shared/Portal/Portal';
 import SVGIcon from '@/components/shared/SVGIcon';
+import Select from '../shared/Select';
 
 import * as Styled from './SearchBar.styled';
 
@@ -12,31 +14,70 @@ interface PoratalPageProps {
 const SearchBar = ({ setOff }: PoratalPageProps) => {
   const router = useRouter();
 
+  const [searchInput, setSearchInput] = useState('');
+  const [searchType, setSearchType] = useState<string>('TITLE');
+
+  const setState =
+    (setSearchParams: React.Dispatch<React.SetStateAction<string>>) =>
+    (value: string) =>
+      setSearchParams((prev) => value);
   return (
     <>
       <Portal selector="#portal">
         <Styled.SearchBarContainer>
-          <Styled.SearchInput autoFocus />
-          <Styled.SearchIconWrapper>
-            <SVGIcon
-              width="1.5em"
-              height="1.55em"
-              icon="SearchIcon"
-              title="검색아이콘"
-              aria-label="검색아이콘"
-              // onClick={} 어떤 것에 대한 검색을 주로 이룰지....
+          <Styled.SelectWrapper>
+            <Styled.DeleteIconWrapper>
+              <SVGIcon
+                width="1em"
+                height="1em"
+                icon="ChevronLeftIcon"
+                title="취소아이콘"
+                aria-label="취소아이콘"
+                onClick={() => setOff()}
+              />
+            </Styled.DeleteIconWrapper>
+            <Select
+              id="1"
+              options={[
+                {
+                  value: 'TITLE',
+                  label: '제목',
+                },
+                {
+                  value: 'CONTENT',
+                  label: '내용',
+                },
+
+                {
+                  value: 'USER',
+                  label: '유저',
+                },
+              ]}
+              defaultLabel="검색 타입"
+              selectedValue={searchType}
+              onChangeOption={setState(setSearchType)}
             />
-          </Styled.SearchIconWrapper>
-          <Styled.DeleteIconWrapper>
-            <SVGIcon
-              width="1.5em"
-              height="1.5em"
-              icon="DeleteIcon"
-              title="취소아이콘"
-              aria-label="취소아이콘"
-              onClick={() => setOff()}
+          </Styled.SelectWrapper>
+          <Styled.SearchInputWrapper>
+            <Styled.SearchInput
+              onChange={(e: any) => {
+                setSearchInput(e.target.value);
+              }}
+              value={searchInput}
             />
-          </Styled.DeleteIconWrapper>
+
+            <Styled.SearchIconWrapper>
+              <SVGIcon
+                width="1.5em"
+                height="1.55em"
+                icon="SearchIcon"
+                title="검색아이콘"
+                aria-label="검색아이콘"
+                onClick={() => router.push(`/search?keyword=${searchInput}`)}
+                // onClick={} 어떤 것에 대한 검색을 주로 이룰지....
+              />
+            </Styled.SearchIconWrapper>
+          </Styled.SearchInputWrapper>
         </Styled.SearchBarContainer>
       </Portal>
     </>
