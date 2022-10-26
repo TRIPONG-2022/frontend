8a0ae5027a7bc755f0a279ee89095af24fe6e1a7
mini <jma1020@naver.com>
2022-10-27 02:01:52 +0900

    refact: useInfinityQuery 형식에 맞도록 api 수정

--- api/search.ts
+++ api/search.ts
@@ -24,22 +24,31 @@ export const postList = async () => {
   return data;
 };
 
-export const getPostList = async (params: {
-  searchType: string | string[] | undefined;
-  keyword: string | string[] | undefined;
-}) => {
-  if (Object.keys(params).length === 0) {
+// 해당 타입이 router.query 로 넘겨줄 때 타입을 찾지 못해주어서... 이렇게 넘겨주고 있다.
+export const getPostList = async (
+  params: {
+    searchType: string | string[] | undefined;
+    keyword: string | string[] | undefined;
+  },
+  pageParam?: number,
+) => {
+  if (params.keyword) {
     const { data } = await axios.get(
       `${process.env.NEXT_PUBLIC_BASE_URL}/search`,
       {
-        params,
+        params: { ...params, page: pageParam, size: 2 },
       },
     );
 
     return data;
   }
 
-  const { data } = await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/posts`);
+  const { data } = await axios.get(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/posts`,
+    {
+      params: { ...params, page: pageParam, size: 2 },
+    },
+  );
 
   return data;
 };
