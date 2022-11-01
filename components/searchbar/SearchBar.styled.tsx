d8dff3be44a6c251d75c310f315c9253bd4506f2
mini <jma1020@naver.com>
2022-11-02 05:01:05 +0900

    refact: Search UI 리팩토링

--- components/searchbar/SearchBar.styled.tsx
+++ components/searchbar/SearchBar.styled.tsx
@@ -1,31 +1,76 @@
 import { HEADER_HEIGHT } from '@/constants/menus';
+import { SCREEN_DESKTOP, SCREEN_TABLET } from '@/styles/screen';
 import { Z_INDEX } from '@/styles/z-index';
 import styled from 'styled-components';
 
 export const SearchBarContainer = styled.div`
   position: fixed;
-  top: 5rem;
+  /* 만약 헤더가 없는 경우에 top 5rem을 적용해줄 예정*/
+  top: 0;
   left: 0;
   right: 0;
-  height: ${HEADER_HEIGHT};
+  bottom: 0;
+
   z-index: ${Z_INDEX.PORTAL_FIXED};
 
+  background-color: rgba(255, 255, 255, 0.5);
+`;
+
+// 만약 헤더가 있을 경우
+// 아닌경우에는 그냥 Icon을 GNB 컴포넌트에서 state 따라 바꿔줄지 생각 중
+export const Header = styled.div`
+  height: 5rem;
+
+  background-color: white;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const LogoDiv = styled.div`
+  width: 50%;
+  height: 100%;
+  margin-left: 5vw;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+
+  svg {
+    cursor: pointer;
+  }
+
+  ${SCREEN_DESKTOP} {
+    width: 25%;
+  }
+`;
+
+export const HeaderDelete = styled.div`
+  margin-right: 5vw;
+`;
+// 헤더 있을 경우
+
+export const Content = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
   gap: 1rem;
 
-  /* border-bottom: solid 1px black; */
+  height: 10rem;
+
   background-color: white;
 `;
 
 export const SelectWrapper = styled.div`
-  min-width: 10rem;
+  min-width: 6rem;
 
   display: flex;
   justify-content: space-between;
   align-items: center;
   gap: 1rem;
+  ${SCREEN_TABLET} {
+    min-width: 10rem;
+  }
 `;
 export const DeleteIconWrapper = styled.div`
   margin-left: 1rem;
@@ -59,6 +104,10 @@ export const SearchInput = styled.input`
     background-color: #fff;
     border-color: ${({ theme }) => theme.colors.primary.hex};
     box-shadow: 0 0 5px rgba(109, 207, 246, 0.5);
+  }
+
+  ::placeholder {
+    font-size: 1rem;
   }
 `;
 
