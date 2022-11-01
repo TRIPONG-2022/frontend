644ddfca1b14f08956be64dd37ac891cb9a652d3
mini <jma1020@naver.com>
2022-11-02 06:21:44 +0900

    fix: 간단한 스타일 수정

--- components/PostCategoryTap/PostCategoryTap.styled.ts
+++ components/PostCategoryTap/PostCategoryTap.styled.ts
@@ -1,15 +1,34 @@
+import { SCREEN_DESKTOP } from '@/styles/screen';
 import styled, { css } from 'styled-components';
 
 export const PostCategoryTapContiner = styled.div`
   display: flex;
-  flex-wrap: wrap;
+  flex-wrap: nowrap;
+  overflow-x: scroll;
+
   gap: 1rem;
   margin-bottom: 1.5rem;
 
   justify-content: start;
+
+  ${SCREEN_DESKTOP} {
+    overflow-x: auto;
+  }
+
+  ::-webkit-scrollbar {
+    height: 4px;
+    width: 4px;
+    background: gray;
+  }
+  ::-webkit-scrollbar-thumb:horizontal {
+    background-color: red;
+    border-radius: 10px;
+  }
 `;
 
 export const Button = styled.button<{ active?: boolean }>`
+  flex-shrink: 0;
+
   padding: 0.875rem 1.125rem;
   border-radius: 2rem;
 
