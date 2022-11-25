c06af60a9f27671d0a14cb766ad6cf0cd7afba8b
sangrimlee <contact@sangrim.dev>
2022-11-25 09:32:46 +0900

    feat: 로그아웃 페이지 추가
    - 로그아웃 함수가 실행되는 로그아웃 페이지 추가

---
+++ pages/auth/logout.tsx
@@ -0,0 +1,21 @@
+import { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import { useDispatch } from 'react-redux';
+
+import { logoutUser } from '@/store/slice/userSlice';
+import useIsomorphicLayoutEffect from '@/hooks/useIsomorphicLayoutEffect';
+
+const LogoutPage: NextPage = () => {
+  const router = useRouter();
+  const dispatch = useDispatch();
+
+  useIsomorphicLayoutEffect(() => {
+    dispatch(logoutUser());
+    window.localStorage.setItem('Authorization', '');
+    router.replace('/');
+  }, []);
+
+  return <div></div>;
+};
+
+export default LogoutPage;
